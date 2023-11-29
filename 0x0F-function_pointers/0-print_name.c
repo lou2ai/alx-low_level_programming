1b27104667edad8a5992d6d113dacab8697aae47
Lou2ai <luai.sulieman@gmail.com>
2023-11-29 10:31:02 -0800

    1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * print_name - prints a name
  * @name: teh string name
@@ -6,6 +7,7 @@
  *
  * Return: void
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
